5edc32f8f9f661a3c534ff4554c8c2302292c978
jruizsilva <ruizsilvajonathan@gmail.com>
2023-10-03 14:10:29 -0300

    constant message changed

--- src/main/java/restdatajpa/common/util/constants/BookConstants.java
+++ src/main/java/restdatajpa/common/util/constants/BookConstants.java
@@ -1,5 +1,5 @@
 package restdatajpa.common.util.constants;
 
 public class BookConstants {
-    public static final String BOOK_NOT_FOUND_MESSAGE_ERROR = "No se encontro un li bro con el id %s";
+    public static final String BOOK_NOT_FOUND_MESSAGE_ERROR = "No se encontro un libro con el id %s";
 }
